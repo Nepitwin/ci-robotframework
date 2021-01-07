66c19ce524e864140639e1f281cbd2fd65404805
Andreas Sekulski <andreas.sekulski@gmail.com>
2021-01-07 18:52:44 +0100

    Testing by bat script

--- test_execution.bat
+++ test_execution.bat
@@ -1,1 +1,16 @@
-robot -x xunit.xml -d result --nostatusrc Automation.robot
+@echo off 
+GOTO:MAIN
+
+:testing
+  robot -x xunit.xml -d result Automation.robot
+EXIT /b %ERRORLEVEL%
+
+:install
+    CALL python -m pip install --upgrade pip
+    CALL pip install wheel
+    CALL pip install robotframework
+    CALL pip install robotframework-whitelibrary
+EXIT /B %ERRORLEVEL%
+
+:MAIN
+IF NOT "%~1" == "" call:%1
